d29739c57261b48b7bb85b45066f074def667d8d
nuwan_ag <nuwan.ag@gmail.com>
2015-04-23 04:36:54 +1000

    csrf_exempt api methods.

--- gvldash/urls.py
+++ gvldash/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from api import get_service, get_services, manage_package, get_packages, manage_system_state
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -36,10 +37,10 @@ urlpatterns = patterns('',
 #     url(r'^avatar/', include('avatar.urls')),
 
     # Your stuff: custom urls go here
-    url(r'^api/v1/services/$', get_services, name='get_services'),
-    url(r'^api/v1/services/(?P<service_name>\w+)/$', get_service, name='get_service'),
-    url(r'^api/v1/packages/$', get_packages, name='get_packages'),
-    url(r'^api/v1/packages/(?P<package_name>\w+)/$', manage_package, name='manage_package'),
-    url(r'^api/v1/system/status/$', manage_system_state, name='manage_system_state'),
+    url(r'^api/v1/services/$', csrf_exempt(get_services), name='get_services'),
+    url(r'^api/v1/services/(?P<service_name>\w+)/$', csrf_exempt(get_service), name='get_service'),
+    url(r'^api/v1/packages/$', csrf_exempt(get_packages), name='get_packages'),
+    url(r'^api/v1/packages/(?P<package_name>\w+)/$', csrf_exempt(manage_package), name='manage_package'),
+    url(r'^api/v1/system/status/$', csrf_exempt(manage_system_state), name='manage_system_state'),
 
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
